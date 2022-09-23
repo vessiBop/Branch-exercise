255d72d9fd2598b2ccf6bcda5e320b167c044fe6
Silvestre Fernandez <rasa356@outlook.com>
2022-09-23 14:23:15 +1200

    another branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import MyButton from "./MyButton.js";
 import Footer from "./Footer.js";
-
+import { useState } from 'react'
 
 
 function App() {
+    const numbers = [1, 2, 3, 4, 5];
+    const [count, setCount] = useState(numbers[1])
+    function updateCount() {
+        setCount((count ** 2))
+    }
+
     return (
         <>
             <h1>Hello World</h1>
             <MyButton></MyButton>
             <p>This is some HTML before the footer</p>
             <p>Some more HTML added on a git branch</p>
+            <h1>Count : {count}</h1>
+            <button onClick={updateCount}>Click Me</button>
+
 
 
             <Footer></Footer>
